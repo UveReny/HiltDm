72e7962b2bb7b3d4319bff1254e64782c1911fa5
MobDe <MobDevX.outlook.com>
2026-04-19 15:20:03 +0700

    Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.sample.hiltdemo"
         minSdk = 27
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
