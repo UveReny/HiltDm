22afc1b48012db5289c7983cf4cb1dbe02355896
MobDe <MobDevX.outlook.com>
2026-04-22 21:31:44 +0700

    start

--- app/src/main/java/com/sample/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/sample/hiltdemo/MainActivity.kt
@@ -4,70 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.sample.hiltdemo.ui.theme.HiltDemoTheme
+import androidx.compose.ui.unit.dp
 import com.sample.hiltdemo.wifi.WiFiManager
 import com.sample.hiltdemo.wifi.WiFiSettings
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val message = "Hello, Android!"
+    private lateinit var wiFiManager: WiFiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HiltDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val message = "Hello, $name"
-
-    LaunchedEffect(Unit) {
-        val mainViewModel = MainViewModel().performWiFiOperations(message)
-    }
-
-    Text(
-        text = message,
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HiltDemoTheme {
-        Greeting("Preview")
-    }
-}
-
-// В ViewModel
-class MainViewModel : ViewModel() {
-    fun performWiFiOperations(message: String) {
-        viewModelScope.launch {
-             val settings = WiFiSettings()
-            val manager = WiFiManager(settings)
-            manager.connect()
-            manager.sendMessage(message)
-            manager.close()
+            val settings = WiFiSettings()
+            wiFiManager = WiFiManager(settings)
+            wiFiManager.connect()
+            wiFiManager.sendMessage(message)
+            wiFiManager.close()
+            Text(text = message, Modifier.padding(32.dp))
         }
     }
 }
