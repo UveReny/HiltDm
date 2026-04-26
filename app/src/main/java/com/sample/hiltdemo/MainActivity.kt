9d691b76697d9b251287132222da462fe9df39b9
MobDe <MobDevX.outlook.com>
2026-04-26 21:25:42 +0700

    Hilt Inject

--- app/src/main/java/com/sample/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/sample/hiltdemo/MainActivity.kt
@@ -4,28 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.sample.hiltdemo.wifi.WiFiManager
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var wiFiManager: WiFiManager
+
     private val message = "Hello, Hilt!"
+    private val text = "Hello, Singleton!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            wiFiManager.connect()
+            Row(Modifier.padding(top = 32.dp)){
+                Text(text = message, Modifier.padding(32.dp))
+            }
+
+            Row(Modifier.padding(top = 128.dp)) {
+                Text(text = text, Modifier.padding(32.dp))
+            }
+/*            wiFiManager.connect()
             wiFiManager.sendMessage(message)
-            wiFiManager.close()
-            Text(text = message, Modifier.padding(32.dp))
+            wiFiManager.close()*/
+
         }
     }
 }
